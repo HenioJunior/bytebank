f732366ea9ccf948a79280eabeffd043587de8fe
Henio Junior <henioajunior@gmail.com>
2021-08-04 10:01:38 -0300

    Utilizando estruturas condicionais

--- src/main.kt
+++ src/main.kt
@@ -5,9 +5,23 @@ fun main() {
     val numeroConta: Int = 1000
     var saldo: Double = 0.0
 
-    saldo += 200
+//    saldo += 200
 
-    println("Titular $titular" )
-    println("Número da conta $numeroConta" )
-    println("Saldo da conta $saldo" )
+    println("Titular $titular")
+    println("Número da conta $numeroConta")
+    println("Saldo da conta $saldo")
+
+//    if (saldo > 0.0) {
+//        println("Conta é positiva")
+//    }else if(saldo == 0.0) {
+//        println("A conta é neutra")
+//    } else {
+//        println("A conta é negativa")
+//    }
+
+    when {
+        saldo > 0.0 -> println("Conta é positiva")
+        saldo == 0.0 -> println("A conta é neutra")
+        else -> println("A conta é negativa")
+    }
 }
