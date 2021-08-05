001830ea293d37e8e886120d23b3cbd5224af394
Henio Junior <henioajunior@gmail.com>
2021-08-05 13:28:40 -0300

    Criando uma nova classe

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,16 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
+    val alex = Funcionario(
+        "Alex",
+        "111.111.111-11",
+        1000.00
+    )
+
+    println("nome ${alex.nome}")
+    println("cpf ${alex.cpf}")
+    println("salario ${alex.salario}")
+    println("bonificacao ${alex.bonificacao()}")
 }
 
 
