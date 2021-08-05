1d6271ded9c008eb004cc938d9c4826bf0c9686e
Henio Junior <henioajunior@gmail.com>
2021-08-05 20:31:28 -0300

    Criando classes para cada funcionário

--- src/main.kt
+++ src/main.kt
@@ -5,12 +5,30 @@ fun main() {
         "Alex",
         "111.111.111-11",
         1000.00
-    )
+   )
 
     println("nome ${alex.nome}")
     println("cpf ${alex.cpf}")
     println("salario ${alex.salario}")
     println("bonificacao ${alex.bonificacao()}")
+
+    val fran = Gerente(
+        "Fran",
+        "222-222-222-22",
+        2000.00,
+        1234
+    )
+    println()
+    println("nome ${fran.nome}")
+    println("cpf ${fran.cpf}")
+    println("salario ${fran.salario}")
+    println("bonificacao ${fran.bonificacao()}")
+
+    if(fran.autentica(1234)) {
+        println("autenticou com sucesso")
+    } else {
+        println("falha na autenticacao")
+    }
 }
 
 
