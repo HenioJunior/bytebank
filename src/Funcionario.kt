b69b99ab1e99670d298fbd2fc782ccc372131292
Henio Junior <henioajunior@gmail.com>
2021-08-06 14:23:51 -0300

    Membros abstratos

--- src/Funcionario.kt
+++ src/Funcionario.kt
@@ -2,15 +2,10 @@ abstract class Funcionario(
     val nome: String,
     val cpf: String,
     val salario: Double,
-    ){
+    ) {
 
     //Funcao convertido para propertie
-    open val bonificacao: Double
-    get() {
-        return salario * 0.1
-    }
-
-
+    abstract val bonificacao: Double
 }
 
 
