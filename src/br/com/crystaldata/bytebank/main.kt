2cc31714b28e1fb069f0c6c31b46484e470dbf5b
Henio Junior <henioajunior@gmail.com>
2021-08-08 09:33:34 -0300

    Implementando o contador

--- src/br/com/crystaldata/bytebank/main.kt
+++ src/br/com/crystaldata/bytebank/main.kt
@@ -1,6 +1,15 @@
+import br.com.crystaldata.bytebank.modelo.Cliente
+import br.com.crystaldata.bytebank.modelo.ContaCorrente
+import br.com.crystaldata.bytebank.modelo.ContaPoupanca
+import br.com.crystaldata.bytebank.modelo.totalContas
+
 fun main() {
 
     testaContasDiferentes()
+    val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
+    val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000)
+    val contaCorrente = ContaCorrente(titular = alex, numero = 1001)
+    println("Total de contas: $totalContas")
 
 }
 
