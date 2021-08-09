ed7403928ccb18842b673f9db996bd14006a8671
Henio Junior <henioajunior@gmail.com>
2021-08-09 11:21:52 -0300

    Limpeza do codigo/Lançando exceptions

--- src/br/com/crystaldata/bytebank/main.kt
+++ src/br/com/crystaldata/bytebank/main.kt
@@ -1,52 +1,9 @@
 import br.com.crystaldata.bytebank.modelo.Endereco
-import java.lang.ArithmeticException
-import java.lang.ClassCastException
-import java.lang.NumberFormatException
 
 fun main() {
     println("início main")
-    val entrada: String = "1.0"
-    val valorRecebido: Double? = try {
-        entrada.toDouble()
-    } catch (e: NumberFormatException) {
-        println("Problema de conversão")
-        e.printStackTrace()
-        null
-    }
-
-//    val valorComTaxa: Double? = if (valorRecebido != null) {
-//        valorRecebido + 0.1
-//    } else {
-//        null
-//    }
-
-//    val valorComTaxa: Double? = when {
-//        valorRecebido != null -> {
-//            valorRecebido + 0.1
-//        }
-//        else -> {
-//            null
-//        }
-//    }
-//
-//    when {valorComTaxa != null -> {
-//        println("Valor recebido: $valorComTaxa")}
-//        else -> {
-//            println("Valor inválido")
-//        }
-//    }
-    val valorComTaxa: Double? = valorComTaxa(1.0)
-    println("Valor recebido: ${valorComTaxa}")
-
     funcao1()
     println("fim main")
-}
-
-fun valorComTaxa(valorRecebido: Double?): Double? {
-    if (valorRecebido != null) {
-        return valorRecebido + 0.1
-    }
-    return null
 }
 
 fun funcao1(){
@@ -55,12 +12,8 @@ fun funcao1(){
         funcao2()
     } catch (e : ClassCastException) {
         e.printStackTrace()
-//        println(e.message)
-//        println(e.stackTrace)
-//        println(e.cause)
         println("ClassCastException")
     }
-
     println("fim funcao1")
 }
 
@@ -69,7 +22,8 @@ fun funcao2() {
     for (i in 1..5) {
         println(i)
         val endereco = Any()
-        endereco as Endereco
+//        endereco as Endereco
+        throw java.lang.ClassCastException()
     }
     println("fim funcao2")
 }
---
+++ src/br/com/crystaldata/bytebank/teste/testaExpressao.kt
@@ -0,0 +1,30 @@
+package br.com.crystaldata.bytebank.teste
+
+fun testaExpressao() {
+    val entrada: String = "1.0"
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema de conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = when {
+        valorRecebido != null -> {
+            valorRecebido + 0.1
+        }
+        else -> {
+            null
+        }
+    }
+
+    when {
+        valorComTaxa != null -> {
+            println("Valor recebido: $valorComTaxa")
+        }
+        else -> {
+            println("Valor inválido")
+        }
+    }
+}
