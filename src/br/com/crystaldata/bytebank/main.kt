c09dc138d965196f25b7c8c2042e6ac9e4f4c34a
Henio Junior <henioajunior@gmail.com>
2021-08-09 16:17:18 -0300

    Utilizando o safe call

--- src/br/com/crystaldata/bytebank/main.kt
+++ src/br/com/crystaldata/bytebank/main.kt
@@ -1,13 +1,20 @@
 import br.com.crystaldata.bytebank.exception.SaldoInsuficienteException
-import br.com.crystaldata.bytebank.modelo.Endereco
+import br.com.crystaldata.bytebank.modelo.Endereco as Endereco1
 
 fun main() {
 
   //(?) indica que podemos receber o valor null...
-    var enderecoNulo: Endereco? = null
-    val enderecoNaoNulo: Endereco = enderecoNulo!!
-    enderecoNaoNulo.logradouro
+    var enderecoNulo: Endereco1? = Endereco1(logradouro = "rua vergueiro")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+
+//    enderecoNulo?.let { endereco: Endereco ->  println(endereco.logradouro)
+//    }
+    enderecoNulo?.let {
+        println(it.logradouro)
+    }
 }
+
+
 
 
 
