7986db94ca0a2ff7ca618b88e2295e4e24da7df8
Henio Junior <henioajunior@gmail.com>
2021-08-09 15:31:26 -0300

    Testando o null

--- src/br/com/crystaldata/bytebank/main.kt
+++ src/br/com/crystaldata/bytebank/main.kt
@@ -2,31 +2,14 @@ import br.com.crystaldata.bytebank.exception.SaldoInsuficienteException
 import br.com.crystaldata.bytebank.modelo.Endereco
 
 fun main() {
-    println("início main")
-//    funcao1()
-    testaComportamentosConta()
-    println("fim main")
+
+  //(?) indica que podemos receber o valor null...
+    var enderecoNulo: Endereco? = null
+    val enderecoNaoNulo: Endereco = enderecoNulo!!
+    enderecoNaoNulo.logradouro
 }
 
-fun funcao1(){
-    println("início funcao1")
-    try {
-        funcao2()
-    } catch (e : SaldoInsuficienteException) {
-        e.printStackTrace()
-        println("SaldoInsuficienteException foi pegada")
-    }
-    println("fim funcao1")
-}
 
-fun funcao2() {
-    println("início funcao2")
-    for (i in 1..5) {
-        println(i)
-        throw SaldoInsuficienteException()
-    }
-    println("fim funcao2")
-}
 
 
 
