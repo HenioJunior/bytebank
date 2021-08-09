ac61acbc5ca87176bdb98daddee52ce369eccadf
Henio Junior <henioajunior@gmail.com>
2021-08-09 10:39:22 -0300

    Utilizando as expressões

--- src/br/com/crystaldata/bytebank/main.kt
+++ src/br/com/crystaldata/bytebank/main.kt
@@ -1,34 +1,56 @@
 import br.com.crystaldata.bytebank.modelo.Endereco
 import java.lang.ArithmeticException
 import java.lang.ClassCastException
+import java.lang.NumberFormatException
 
 fun main() {
     println("início main")
-//    try {
-//        10/0
-//    }catch (e: ArithmeticException) {
-//        println("ArithmeticException foi pegada")
-//    }
+    val entrada: String = "1,0"
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema de conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorRecebido != null) {
+        valorRecebido + 0.1
+    } else {
+        null
+    }
+
+    if(valorComTaxa != null){
+        println("Valor recebido: $valorComTaxa")
+    } else {
+        println("Valor inválido")
+    }
+
     funcao1()
     println("fim main")
 }
 
 fun funcao1(){
     println("início funcao1")
-    funcao2()
+    try {
+        funcao2()
+    } catch (e : ClassCastException) {
+        e.printStackTrace()
+//        println(e.message)
+//        println(e.stackTrace)
+//        println(e.cause)
+        println("ClassCastException")
+    }
+
     println("fim funcao1")
 }
 
 fun funcao2() {
     println("início funcao2")
-    try {
-        for (i in 1..5) {
-            println(i)
-            val endereco = Any()
-            endereco as Endereco
-        }
-    } catch(e : ClassCastException) {
-        println("ClassCastException")
+    for (i in 1..5) {
+        println(i)
+        val endereco = Any()
+        endereco as Endereco
     }
     println("fim funcao2")
 }
