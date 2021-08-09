2308cf920b0f165d34b938ddd46d22b415031470
Henio Junior <henioajunior@gmail.com>
2021-08-08 21:32:31 -0300

    Sobrescrevendo o equals e o hashcode

--- src/br/com/crystaldata/bytebank/main.kt
+++ src/br/com/crystaldata/bytebank/main.kt
@@ -2,17 +2,27 @@ import br.com.crystaldata.bytebank.modelo.*
 
 fun main() {
 
-    val endereco = Endereco(logradouro = "Rua vergueiro", complemento = "Alura")
-    val enderecoNovo = Endereco(bairro = "Vila Mariana", numero = 1000)
+    val endereco = Endereco(
+        logradouro = "Rua vergueiro",
+        complemento = "Alura",
+        cep = "00000-0700"
+    )
+    val enderecoNovo = Endereco(
+        logradouro = "Rua vergueiro",
+        complemento = "Alura",
+        cep = "00000-0700"
+    )
+
+    println(endereco.equals(enderecoNovo))
 
     println(endereco.hashCode())
     println(enderecoNovo.hashCode())
 
-    println(endereco)
-    println(enderecoNovo)
-
-    println("${endereco.javaClass}@${
-        Integer.toHexString(endereco.hashCode())}")
+//    println(endereco)
+//    println(enderecoNovo)
+//
+//    println("${endereco.javaClass}@${
+//        Integer.toHexString(endereco.hashCode())}")
 
 }
 
