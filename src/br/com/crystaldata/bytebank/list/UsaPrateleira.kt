106d177d3ab4b04bb390b77b57e2880d629a909c
Henio Junior <henioajunior@gmail.com>
2021-08-10 11:10:18 -0300

    Lidando com mutabilidade

---
+++ src/br/com/crystaldata/bytebank/list/UsaPrateleira.kt
@@ -0,0 +1,11 @@
+package br.com.crystaldata.bytebank.list
+
+fun main() {
+    val prateleira = Prateleira(genero = "Literatura", livros = listaDeLivros)
+
+    val porAutor = prateleira.organizarPorAutor()
+    val porAnoPublicacao = prateleira.organizarPorAnoPublicacao()
+
+    porAutor.imprimeComMarcadores()
+    porAnoPublicacao.imprimeComMarcadores()
+}
