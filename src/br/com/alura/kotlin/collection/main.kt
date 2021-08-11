1621359cb37d636cb98f4671343d54309cbc7e92
Henio Junior <henioajunior@gmail.com>
2021-08-10 21:39:48 -0300

    Utilizando o Set

--- src/br/com/alura/kotlin/collection/main.kt
+++ src/br/com/alura/kotlin/collection/main.kt
@@ -1,9 +1,21 @@
 package br.com.alura.kotlin.collection
 
 fun main() {
-    val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
-    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
-    val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
-    println(assistiramAmbos.distinct())
+    val assistiramCursoAndroid: Set<String> = setOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin: Set<String> = setOf("Alex", "Paulo", "Maria")
+//    val assistiramAmbos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
+    val assistiramAmbos = mutableSetOf<String>()
 
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    println(assistiramAmbos)
+    println()
+
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    println(assistiramAmbos)
+    println()
+
+    assistiramAmbos.add("Ana")
+    assistiramAmbos.add("Ana")
+    println(assistiramAmbos)
+    println()
 }
