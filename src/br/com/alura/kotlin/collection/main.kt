03f611df33d93d829f9ac3883899c6986b84a5a2
Henio Junior <henioajunior@gmail.com>
2021-08-10 21:21:03 -0300

    Copiando coleções

--- src/br/com/alura/kotlin/collection/main.kt
+++ src/br/com/alura/kotlin/collection/main.kt
@@ -3,13 +3,15 @@ package br.com.alura.kotlin.collection
 fun main() {
 
     val banco = BancoDeNomes()
+    val nomesSalvos: Collection<String> = banco.nomes
     banco.salva("Alex")
     println(banco.nomes)
+    println(nomesSalvos)
     println(BancoDeNomes().nomes)
 }
 
 class BancoDeNomes {
-    val nomes: Collection<String> get() = dados
+    val nomes: Collection<String> get() = dados.toList()
 
     fun salva(nome: String){
         dados.add(nome)
